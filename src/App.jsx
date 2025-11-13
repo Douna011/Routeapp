794c3520a816b12d6d5d62bd3f1d162088eadfc1
John Doe <johndoe@example.com>
2025-11-13 13:42:13 +0700

    tailwind add

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
 
   return <div className=" flex justify-center  w-0.5/2 bg-blue-400 p-2 ">
-    <h2 className="font-bold bg-green-500 p-2 w-100 flex justify-center items-center rounded-2xl border-2">Hello world</h2>
+    <h2 className="font-bold bg-green-500 p-2 w-100 flex justify-center items-center rounded-2xl border-2 " >Hello world</h2>
   </div> 
       
 }
