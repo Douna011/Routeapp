9b00849b07591bf5bec66b88bdfeee35f5b3c8d2
John Doe <johndoe@example.com>
2025-11-14 00:10:32 +0700

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
-import { useState } from 'react'
+import './App.css';
 
-import './App.css'
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path:"/",
+    element:<h2>Home Page</h2>,
+  },
+  {
+    path:"/about",
+    element:<h2>About Page</h2>,
+  },
+  {
+    path:"/service",
+    element:<h2>Service Page</h2>,
+  }
+]);
+
+
 
 function App() {
 
 
-  return <div className=" flex justify-center  w-0.5/2 bg-blue-400 p-2 ">
+  return <div className=" flex justify-center  w-0.5/2 bg-blue-400 p-2  ">
+     <RouterProvider router={router} />
     <h2 className="font-bold bg-green-500 p-2 w-100 flex justify-center items-center rounded-2xl border-2">Hello world</h2>
   </div> 
+  
       
 }
 
